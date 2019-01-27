81986869bac5f05720b25fd933d81ea8f0b2c357
Jimmy Huang <jimmyhuang0904@hotmail.com>
2019-01-27 00:24:30 -0800

    Need to fix backend integration

--- recorder/index.js
+++ recorder/index.js
@@ -9,9 +9,9 @@ const client = new speech.SpeechClient();
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
  */
-const encoding = 'Encoding of the audio file, e.g. LINEAR16';
+const encoding = 'LINEAR16';
 const sampleRateHertz = 16000;
-const languageCode = 'BCP-47 language code, e.g. en-US';
+const languageCode = 'en-US';
 
 const request = {
   config: {
@@ -27,7 +27,7 @@ const recognizeStream = client
   .streamingRecognize(request)
   .on('error', console.error)
   .on('data', data =>
-    process.stdout.write(
+    console.log(
       data.results[0] && data.results[0].alternatives[0]
         ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
         : `\n\nReached transcription time limit, press Ctrl+C\n`
@@ -50,20 +50,24 @@ record
 console.log('Listening, press Ctrl+C to stop.');
 
 const { exec } = require('child_process');
-exec('sox -b 16 -r 16k -c 1 -d ./nick_test.wav trim 0 2 && ../Identification/IdentifyFile.py nick_test.wav True 03f5cadf-309f-4228-8390-05007eb83ece 5d545b81-a3d9-4ea7-be55-aba94c7c1a05 cd74bc8f-71c5-46cf-82d6-9f3f30dadc30 157e954b-6fe7-4d96-a9ac-f1135521e9fa', (err, stdout, stderr) => {
+function findSpeaker() {
+  exec('sox -b 16 -r 16k -c 1 -d ./nick_test.wav trim 0 2 && ../Identification/IdentifyFile.py nick_test.wav True 03f5cadf-309f-4228-8390-05007eb83ece 5d545b81-a3d9-4ea7-be55-aba94c7c1a05 cd74bc8f-71c5-46cf-82d6-9f3f30dadc30 157e954b-6fe7-4d96-a9ac-f1135521e9fa', (err, stdout, stderr) => {
 
 
-  // if (err) {
-  //   // node couldn't execute the command
-  //   return;
-  // }
+    // if (err) {
+    //   // node couldn't execute the command
+    //   return;
+    // }
 
-  // the *entire* stdout and stderr (buffered)
-  // console.log(`stdout: ${stdout}`);
+    // the *entire* stdout and stderr (buffered)
+    // console.log(`stdout: ${stdout}`);
 
-  var lines = stdout.split('\n')
-  var speakerId = lines[0].split('=')[1].trim()
-  var confidence = lines[1].split('=')[1].trim()
-  console.log(speakerId, confidence)
-  // console.log(`stderr: ${stderr}`);
-});
+    var lines = stdout.split('\n')
+    var speakerId = lines[0].split('=')[1].trim()
+    var confidence = lines[1].split('=')[1].trim()
+    console.log(speakerId, confidence)
+    // console.log(`stderr: ${stderr}`);
+  });
+}
+
+// findSpeaker();
