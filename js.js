245672a55020bcf5296df32d19796a00f0b59366
Nick Wu <nickwu241@gmail.com>
2019-01-27 00:49:10 -0800

    Add endpoint for voice

--- js.js
+++ js.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var hbs = require('hbs');
 const express = require('express');
 const fetch = require("node-fetch");
+const { exec } = require('child_process');
 
 const app = express();
 
@@ -28,9 +29,28 @@ app.get('/', (req, res) => {
 	});
 });
 
+cmd = 'sox -b 16 -r 16k -c 1 -d ./nick_test.wav trim 0 2 && ./Identification/IdentifyFile.py nick_test.wav True 03f5cadf-309f-4228-8390-05007eb83ece 5d545b81-a3d9-4ea7-be55-aba94c7c1a05 cd74bc8f-71c5-46cf-82d6-9f3f30dadc30 157e954b-6fe7-4d96-a9ac-f1135521e9fa',
+// cmd = './Identification/IdentifyFile.py nick_test.wav True 03f5cadf-309f-4228-8390-05007eb83ece 5d545b81-a3d9-4ea7-be55-aba94c7c1a05 cd74bc8f-71c5-46cf-82d6-9f3f30dadc30 157e954b-6fe7-4d96-a9ac-f1135521e9fa',
+
+app.post('/speakerId', (req, res) => {
+    exec(cmd ,(err, stdout, stderr) => {
+        if (err) {
+            console.error('couldnt execute command', err);
+            throw err;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+
+        var lines = stdout.split('\n')
+        var speakerId = lines[0].split('=')[1].trim()
+        var confidence = lines[1].split('=')[1].trim()
+        res.json({speakerId: speakerId, confidence: confidence});
+      });
+})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server up on port ${port}`)
     setInterval(sendRequest, 1000)
-    
 });
