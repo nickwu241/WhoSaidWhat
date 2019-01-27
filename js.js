c5bfac50acfaff51d8138e9f7375e44ecfa83554
Sorina <schirhei@my.bcit.ca>
2019-01-26 21:28:10 -0800

    site is ready to go

--- js.js
+++ js.js
@@ -8,26 +8,29 @@ const app = express();
 
 
 const port = process.env.port || 8080;
-var data = {};
+var tra = '';
+var na = '';
 
 function sendRequest(){
     fetch('https://jsonplaceholder.typicode.com/todos/')
     .then(response => response.json())
     .then(json => {
-        data = json[Math.floor(Math.random() * 100) + 1 ] 
+        data = json[Math.floor(Math.random() * 100) + 1 ];
+        tra += data.title;
+        na = data.userId;
     })
 }
 
 app.get('/', (req, res) => {
     res.render('index.hbs', {
-        transcript: data.title,
-        name: data.userId
+        transcript: tra,
+        name: na
 	});
 });
 
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server up on port ${port}`)
-    setInterval(sendRequest, 100)
+    setInterval(sendRequest, 1000)
     
 });
