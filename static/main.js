3f75b81cf512e2c04621085e2b1ef55005447a8f
Nick Wu <nickwu241@gmail.com>
2019-01-27 10:21:13 -0800

    Recreate frontend using Vue

---
+++ static/main.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        transcript: [],
+        currentSpeaker: '',
+    },
+    computed: {
+        speakerClass() {
+            return {
+                'speaker': true,
+                'mickey': this.currentSpeaker === 'Mickey',
+                'jimmy': this.currentSpeaker === 'Jimmy',
+                'sorina': this.currentSpeaker === 'Sorina'
+            }
+        },
+        speechClass() {
+            return {
+                'speech': true,
+            }
+
+        }
+    },
+    methods: {
+        getTranscript() {
+            fetch('/transcript').then(r => r.json()).then(res => {
+                this.transcript = res['transcript'];
+                this.currentSpeaker = res['currentSpeaker'];
+            }).catch(err => console.error(err));
+        }
+    },
+    mounted() {
+        setInterval(this.getTranscript, 1000);
+    }
+})
