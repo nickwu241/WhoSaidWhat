92a947616ced5bace45707e3ba7bcc5be8e07dbd
Saketh Nimmagadda <sakethn@cs.washington.edu>
2019-01-27 11:41:57 -0800

    Add New User button creates dialog box allowing you to add new user

--- static/main.js
+++ static/main.js
@@ -34,6 +34,35 @@ var app = new Vue({
         }
     },
     mounted() {
+        var modal = document.getElementById('myModal');
+        
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        var recBtn = document.getElementById("recordButton");
+        
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        // When the user clicks on the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+        
+        recBtn.onclick = function() {
+            recBtn.disabled = true;
+        }
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
         setInterval(this.getTranscript, 1000);
     }
 })
